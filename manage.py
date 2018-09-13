7f13fb3564b57aed535c174e12bd50c61202c477
Owenmur21 <owenmuriithi@gmail.com>
2018-09-13 10:44:18 +0300

    imports db

--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-from app import create_app
+from app import create_app,db
 from flask_script import Manager,Server
 
 
@@ -14,6 +14,6 @@ manager.add_command('server',Server)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app)
+    return dict(app = app, db = db)
 if __name__ == '__main__':
     manager.run()
