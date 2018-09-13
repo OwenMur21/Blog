4cc9a3f82242e3471b1d1449fc4e9695a6282fe5
Owenmur21 <owenmuriithi@gmail.com>
2018-09-13 10:59:43 +0300

    Creates User class

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') 
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://username:password@localhost/blog'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ozzy:ozzie21@localhost/blog'
 
  
 
--- manage.py
+++ manage.py
@@ -1,5 +1,7 @@
 from app import create_app,db
 from flask_script import Manager,Server
+from  flask_migrate import Migrate, MigrateCommand
+from app.models import User
 
 
 
@@ -7,13 +9,17 @@ from flask_script import Manager,Server
 app = create_app('development')
 # app = create_app('production')
 # app = create_app('test')
+
 manager = Manager(app)
 manager.add_command('server',Server)
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
 
 
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app, db = db)
+    return dict(app = app, db = db, User = User)
 if __name__ == '__main__':
     manager.run()
