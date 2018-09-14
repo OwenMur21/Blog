555f5a7ca6a04e921a6d8f9cb8c8f062d50fc7dc
Owenmur21 <owenmuriithi@gmail.com>
2018-09-15 01:16:02 +0300

    Creates forms for posting a blog and commenting

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,8 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField,TextAreaField,RadioField
+from wtforms import StringField,PasswordField,SubmitField,TextAreaField
 from wtforms.validators import Required
 from wtforms import ValidationError
 
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [Required()])
     submit = SubmitField('Submit')
+
+class PitchForm(FlaskForm):
+    content = TextAreaField("What is your blog about?",validators=[Required()])
+    submit = SubmitField('Submit')
+
+class CommentForm(FlaskForm):
+    description = TextAreaField('Add comment',validators=[Required()])
+    submit = SubmitField()
