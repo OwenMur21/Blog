9aae3e373c1688182c74b8c160834ee41d36312a
Owenmur21 <owenmuriithi@gmail.com>
2018-09-16 11:08:32 +0300

    Adds bio to user class

--- app/models.py
+++ app/models.py
@@ -18,6 +18,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(255))
     email = db.Column(db.String(255),unique = True,index = True)
     pass_secure = db.Column(db.String(255))
+    bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
     blogs = db.relationship('Blog', backref='user', lazy='dynamic')
     comments = db.relationship('Comments', backref = 'user', lazy = 'dynamic')
